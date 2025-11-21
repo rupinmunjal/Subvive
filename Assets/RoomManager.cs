f76ed484f8d7c2aab54c1ead15f6004528009da2
vpak1 <133903182+vpak1@users.noreply.github.com>
2025-11-21 13:33:35 -0500

    Add initial multiplayer feature

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+using System;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public GameObject player1;
+    public GameObject player2;
+    
+    [Space]
+    public Transform spawnPoint1;
+    
+    [Space]
+    public Transform spawnPoint2;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+        
+    }
+    
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        
+        Debug.Log("Connected to Server");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        
+        Debug.Log("Joined Lobby");
+        
+        PhotonNetwork.JoinOrCreateRoom("test", new RoomOptions { MaxPlayers = 2 }, TypedLobby.Default);
+        
+    }
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        
+        Debug.Log("Joined Room");
+        
+        GameObject selectedPrefab;
+        Transform selectedSpawn;
+        
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
+        {
+            selectedPrefab = player1;
+            selectedSpawn = spawnPoint1;
+        }
+        else
+        {
+            selectedPrefab = player2;
+            selectedSpawn = spawnPoint2;
+        }
+        
+        GameObject _player = PhotonNetwork.Instantiate(selectedPrefab.name, selectedSpawn.position, Quaternion.identity);
+        
+    }
+}
